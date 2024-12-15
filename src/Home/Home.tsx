53e3ad8fd4e7f037f2d4ea3ae47f3fcb7a6585dc
samu <1936709+samu@users.noreply.github.com>
2024-12-15 15:07:17 +0100

    fix description

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,13 +1,12 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import { BackendAndClientSection } from "./sections/BackendAndClientSection/BackendAndClientSection";
-import { Header } from "./sections/Header/Header";
 import { PipelineSection } from "./sections/FastSection/FastSection";
+import { Header } from "./sections/Header/Header";
 import { TypeSafetySection } from "./sections/TypeSafetySection/TypeSafetySection";
 
 export default function Home(): JSX.Element {
   return (
-    <Layout description="Kinekt is a Web Framework written in TypeScript.">
+    <Layout description="A TypeScript-first web framework for building the next generation of web apps.">
       <Header></Header>
 
       <main className="home-main">
